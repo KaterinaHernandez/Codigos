8120cb4e060f7564958e7092f73c78740bf27d54
KaterinaHernandez <katerinahernandez1@gmail.com>
2014-01-11 13:42:07 -0300

    Revert "Enfoca el primer elemento"
    This reverts commit adc2aacfa746cf95fa78b0b38e5a3a968baeda7f.

--- EnfocaElPrimerElemento.js
+++
@@ -1,8 +0,0 @@
-//asigna automáticamente el foco del programa al primer elemento del primer formulario de la página,
-// independientemente del id del formulario y de los elementos:
-
-if(document.forms.length > 0) {
-  if(document.forms[0].elements.length > 0) {
-    document.forms[0].elements[0].focus();
-  }
-}
